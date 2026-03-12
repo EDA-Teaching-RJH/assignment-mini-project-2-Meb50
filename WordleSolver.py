22a6b48864e7bb54bbb2f3a8119516c20a51f378
Meb50 <meb50@kent.ac.uk>
2026-03-12 17:53:34 +0000

    Created a class which should filter and suggest words

--- WordleSolver.py
+++ WordleSolver.py
@@ -2,7 +2,7 @@ import words
 
 class WordList:
     def __init__(self, filepath):
-        self.Words = self.load_words(filepath)
+        self.words = self.load_words(filepath)
     
     def load_words(self, filepath):
         with open(filepath) as file:
@@ -17,3 +17,13 @@ class WordleGame:
         self.history.append(guess, result)
     #Stores guesses and results
 
+class WordleSolver:
+    def __init__(self, words):
+        self.candidates = words
+
+    def apply_guess(self, guess, result):
+        pass
+
+    def suggest_word(self):
+        pass
+    
