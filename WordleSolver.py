0d5fce67e3ed679298f2f00a62fd01b88bea7522
Meb50 <meb50@kent.ac.uk>
2026-03-12 17:48:50 +0000

    Made a class which stores guesses and results from the game

--- WordleSolver.py
+++ WordleSolver.py
@@ -5,6 +5,15 @@ class WordList:
         self.Words = self.load_words(filepath)
     
     def load_words(self, filepath):
-        with open(filepath, "r") as file:
+        with open(filepath) as file:
             return [word.strip() for word in file if len(word.strip()) == 5]
         #Loads 5 letter words from a file.
+
+class WordleGame:
+    def __init__(self):
+        self.history = [ ]
+    
+    def add_guess(self, guess, result):
+        self.history.append(guess, result)
+    #Stores guesses and results
+
